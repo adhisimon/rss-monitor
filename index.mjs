1a81e45edf25231184cfccafc72313f4d48f8ef0
Adhidarma Hadiwinoto <me@adhisimon.org>
2025-06-09 18:19:24 +0700

    Begin working on webui

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import logger from './lib/logger.mjs';
+import { initWebUI } from './lib/webui/index.mjs';
 
 const MODULE_NAME = 'MAIN';
 
 logger.info(`${MODULE_NAME} 3CB79FBE: Starting`);
+await initWebUI();

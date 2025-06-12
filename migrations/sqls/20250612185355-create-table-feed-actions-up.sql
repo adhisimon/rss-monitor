88f23956fbbca65f9cd07a57145f38002e0ad875
Adhidarma Hadiwinoto <me@adhisimon.org>
2025-06-13 02:39:36 +0700

    FK feed_uuid on feed_actions

--- migrations/sqls/20250612185355-create-table-feed-actions-up.sql
+++ migrations/sqls/20250612185355-create-table-feed-actions-up.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS `feed_actions` (
   `last_action` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`uuid`)
 ) ENGINE = InnoDB;
+
+ALTER TABLE `feed_actions` ADD UNIQUE IF NOT EXISTS `feed_action` (`feed_uuid`, `action`);
+
+ALTER TABLE `feed_actions` 
+  ADD CONSTRAINT `feed_actions_fk_feed_uuid`
+    FOREIGN KEY IF NOT EXISTS (`feed_uuid`)
+    REFERENCES `feeds`(`uuid`) 
+    ON DELETE CASCADE ON UPDATE CASCADE;
